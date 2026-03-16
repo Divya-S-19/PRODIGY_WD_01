1c2769007aa250f42b4c2e5210275a92bb23b99b
Divya S <divya19.saravanapandian@gmail.com>
2026-03-16 23:33:34 +0530

    updated project code

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ window.addEventListener("scroll", function(){
 
 let navbar = document.getElementById("navbar");
 
-if(window.scrollY > 50){
-navbar.classList.add("scrolled");
+if(window.scrollY > 50)
+{
+navbar.classList.add("navbar-active");
 }
-else{
-navbar.classList.remove("scrolled");
+else
+{
+navbar.classList.remove("navbar-active");
 }
 
 });
